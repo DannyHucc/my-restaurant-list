98cd013371ac850ec8915c9c762a927bff283c5c
DannyHucc <dannyhu10011101@gmail.com>
2023-05-01 20:59:30 +0800

    feat: add mongoose

---
+++ config/mongoose.js
@@ -0,0 +1,21 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+db.on('error', () => {
+    console.log('mongodb error!')
+})
+
+db.once('open', () => {
+    console.log('mongodb connected!')
+})
