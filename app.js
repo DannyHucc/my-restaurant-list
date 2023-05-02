fba0843eb0601eea99ddcdfe4132b3f2456807e1
DannyHucc <dannyhu10011101@gmail.com>
2023-05-02 20:22:00 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
@@ -7,10 +8,7 @@ const port = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
-
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.use(routes)
 
 app.listen(port, () => {
     console.log(`Express is listening on localhost:${port}`)
