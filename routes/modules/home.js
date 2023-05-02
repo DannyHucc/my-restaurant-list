1ddd27cf8d2f8e7e1def7914e5826aca43815e89
DannyHucc <dannyhu10011101@gmail.com>
2023-05-02 21:04:29 +0800

    feat: modify home.js and main.hbs and index.hbs

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const Restaurants = require('models-file/restaurant')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('index')
+    Restaurants.find()
+        .lean()
+        .then(restaurants => res.render('index', { restaurants }))
+        .catch(error => console.error(error))
 })
 
 module.exports = router
