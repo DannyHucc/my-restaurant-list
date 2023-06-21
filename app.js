ff30780e35f7af3cd984d0c27aba1065027e824e
DannyHucc <dannyhu10011101@gmail.com>
2023-06-21 13:13:42 +0800

    feat: add register function

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express')
 const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
 
-app.listen(port, () => {
-    console.log(`Express is listening on localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`Express is listening on localhost:${PORT}`)
 })
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
+const User = require('models-file/user')
 const router = express.Router()
 
 router.get('/login', (req, res) => {
@@ -7,6 +9,25 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
     res.render('register')
+})
+
+router.post('/register', async (req, res) => {
+    try {
+        const { name, email, password, confirmPassword } = req.body
+
+        const isUserExisted = await User.exists({ email })
+        if (isUserExisted) {
+            console.log('User already exists.')
+            return res.render('register', { name, email, password, confirmPassword })
+        }
+
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(password, salt, null)
+        await User.create({ name, email, password: hash })
+        return res.redirect('/users/login')
+    } catch (error) {
+        console.error(error)
+    }
 })
 
 module.exports = router
