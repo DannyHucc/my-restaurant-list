9d9aa08b22fdf955d6766ac4840ddcef8d2efbc5
DannyHucc <dannyhu10011101@gmail.com>
2023-06-22 12:15:30 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const usePassport = require('config-file/passport')
+const flash = require('connect-flash')
 const routes = require('./routes')
 require('./config/mongoose')
 const app = express()
@@ -36,10 +37,13 @@ app.use(session({
 // middleware: passport initialize
 usePassport(app)
 
-// middleware: authenticate
+// middleware: flash and authenticate
+app.use(flash())
 app.use((req, res, next) => {
     res.locals.isAuthenticated = req.isAuthenticated()
     res.locals.user = req.user
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.warning_msg = req.flash('warning_msg')
     next()
 })
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -20,16 +20,36 @@ router.get('/register', (req, res) => {
 router.post('/register', async (req, res) => {
     try {
         const { name, email, password, confirmPassword } = req.body
+        const errors = []
+        const passwordRegex = 
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*()_+-={}\[\]\\:;"'<>?,.\/])[A-Za-z\d~!@#$%^&*()_+-={}\[\]\\:;"'<>?,.\/]{8,20}$/
+
+        if (!email.trim() || !passport || !confirmPassword) {
+            errors.push({ message: 'Please fill in email, password, and confirm password fields.' })
+        }
+
+        if (!passwordRegex.test(password)) {
+            errors.push({ message: 'The password must be 8-20 characters long, contain letters and numbers and symbols.' })
+        }
+
+        if (password !== confirmPassword) {
+            errors.push({ message: 'The password confirmation dose not match.' })
+        }
+
+        if (errors.length) {
+            return res.render('register', { errors, name, email, password, confirmPassword })
+        }
 
         const isUserExisted = await User.exists({ email })
         if (isUserExisted) {
-            console.log('User already exists.')
-            return res.render('register', { name, email, password, confirmPassword })
+            errors.push({ message: 'User already exists!' })
+            return res.render('register', { errors, name, email, password, confirmPassword })
         }
 
         const salt = bcrypt.genSaltSync(10)
         const hash = bcrypt.hashSync(password, salt, null)
         await User.create({ name, email, password: hash })
+        req.flash('success_msg', 'Register successfully! Please login to your account.')
         return res.redirect('/users/login')
     } catch (error) {
         console.error(error)
@@ -38,6 +58,7 @@ router.post('/register', async (req, res) => {
 
 router.get('/logout', (req, res) => {
     req.logout()
+    req.flash('success_msg', 'You have successfully logged out.')
     res.redirect('/users/login')
 })
 
