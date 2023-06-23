6a8372e4db6fa7fce842fcfe88532f58942827c8
DannyHucc <dannyhu10011101@gmail.com>
2023-06-23 11:56:42 +0800

    modify search function

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -4,13 +4,19 @@ const Restaurants = require('models-file/restaurant')
 
 router.get('/', (req, res) => {
     const keywords = req.query.keywords
+    const userId = req.user._id
     const sort = req.query.sort
 
     Restaurants.find({
-        $or: [
-            { name: { $regex: keywords, $options: 'xi' } },
-            { name_en: { $regex: keywords, $options: 'xi' } },
-            { category: { $regex: keywords, $options: 'xi' } }
+        $and: [
+            { userId },
+            {
+                $or: [
+                    { name: { $regex: keywords, $options: 'xi' } },
+                    { name_en: { $regex: keywords, $options: 'xi' } },
+                    { category: { $regex: keywords, $options: 'xi' } }
+                ]
+            }
         ]
     })
         .lean()
