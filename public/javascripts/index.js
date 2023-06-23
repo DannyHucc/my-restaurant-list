2857b6cbfbbab1f7cb9d02c9115d2a141db2b28e
DannyHucc <dannyhu10011101@gmail.com>
2023-06-23 15:30:55 +0800

    feat: add alter function for delete in home page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,30 @@
+const cards = document.querySelector('.restaurant-cards')
 const sort = document.querySelector('select').id
 const options = [...document.querySelectorAll('option')]
 
+// option select for sort
 options.find((option) => {
     if (option.value === sort) option.selected = true
 })
+
+function deleteRestaurant(event) {
+    const target = event.target
+    if ((target.matches('.btn-delete') || target.matches('.fa-trash-alt'))) {
+        event.preventDefault()
+        swal({
+            title: "確定刪除資料?",
+            icon: "warning",
+            text: "刪除的資料將無法恢復",
+            buttons: true,
+            dangerMode: true
+        }).then(check => {
+            if (check) {
+                const id = target.dataset.id
+                document.querySelector(`.form-${id}`).submit()
+            }
+        })
+    }
+
+}
+
+cards.addEventListener('click', deleteRestaurant)
